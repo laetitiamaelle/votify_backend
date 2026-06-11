472ae79d1d8f4c16a4b4ccef7d73726b0c76abd8
laetitiamaelle <laetitiamaelle740@gmail.com>
2026-06-11 20:39:47 +0100

    ba

--- votify_backend/settings.py
+++ votify_backend/settings.py
@@ -98,8 +98,8 @@ WSGI_APPLICATION = 'votify_backend.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
         conn_max_age=600,
+        ssl_require=True
     )
 }
 
