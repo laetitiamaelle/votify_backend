9e90f470664b3ec835e046e0deec7b06fbbbdd0e
laetitiamaelle <laetitiamaelle740@gmail.com>
2026-06-11 19:57:05 +0100

    baba

--- votify_backend/settings.py
+++ votify_backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
